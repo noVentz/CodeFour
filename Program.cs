1a9c52f59b818fe8d5c69ba00cd49aff9d800715
noVentz <103551295+noVentz@users.noreply.github.com>
2022-04-13 17:52:08 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeFour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\t Введите вашу кликуху");
+            string name = Console.ReadLine();
+            Console.WriteLine("А теперь сколько тебе годиков?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Короче, твоё погоняло {name} и сейчас тебе уже {age}");
+        }
+    }
+}
